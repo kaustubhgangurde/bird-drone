22c5839bbc92dddff25b08146ba686825b17c8b2
kaustubh <kaustubhgangurde15@gmail.com>
2026-07-08 07:21:36 +0530

    Added firmware skeleton

---
+++ firmware/src/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <Arduino.h>
+
+// TODO: Include Camera libraries
+// TODO: Include WiFi libraries
+
+void setup() {
+  Serial.begin(115200);
+  // TODO: Initialize ESP32-CAM
+  // TODO: Setup WiFi Access Point (AP)
+  // TODO: Start MJPEG Video Streaming server
+  // TODO: Setup UDP/TCP listening socket for control commands
+  // TODO: Attach PWM signals for Main Motor and Tail Servo
+}
+
+void loop() {
+  // TODO: Handle incoming control packets from Mobile App
+  // TODO: Update Motor PWM and Servo angles based on commands
+  delay(10);
+}
